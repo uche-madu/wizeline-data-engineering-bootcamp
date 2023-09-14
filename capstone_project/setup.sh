d6f2b994ebe71663a3e537a841c108bab0cf2dff
Uche Madu <dreemer6@yahoo.com>
2023-09-14 06:25:34 +0100

    clean up setup.sh

--- capstone_project/setup.sh
+++ capstone_project/setup.sh
@@ -13,7 +13,7 @@ gcloud iam service-accounts create deb-sa \
     --description="Service Account for Wizeline Data Engineering Bootcamp"
 
 # Generate and Download JSON Key
-gcloud iam service-accounts keys create .credentials.json \
+gcloud iam service-accounts keys create credentials.json \
     --iam-account="deb-sa@${PROJECT_ID}.iam.gserviceaccount.com"
 
 # Set the Service Account Key File Environment Variable
@@ -82,35 +82,3 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} \
 gcloud services enable container.googleapis.com \
     storage-component.googleapis.com \
     dataproc.googleapis.com
-
-# gcloud beta container clusters create "cluster-1" \
-#     --zone "us-central1-c" \
-#     --no-enable-basic-auth \
-#     --cluster-version "1.27.3-gke.100" \
-#     --release-channel "regular" \
-#     --machine-type "e2-medium" \
-#     --image-type "COS_CONTAINERD" \
-#     --disk-type "pd-balanced" \
-#     --disk-size "100" \
-#     --metadata disable-legacy-endpoints=true \
-#     --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-#     --num-nodes "3" \
-#     --logging=SYSTEM,WORKLOAD \
-#     --monitoring=SYSTEM \
-#     --enable-ip-alias \
-#     --network "projects/wizeline-deb/global/networks/default" \
-#     --subnetwork "projects/wizeline-deb/regions/us-central1/subnetworks/default" \
-#     --no-enable-intra-node-visibility \
-#     --default-max-pods-per-node "110" \
-#     --security-posture=standard \
-#     --workload-vulnerability-scanning=disabled \
-#     --no-enable-master-authorized-networks \
-#     --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
-#     --enable-autoupgrade \
-#     --enable-autorepair \
-#     --max-surge-upgrade 1 \
-#     --max-unavailable-upgrade 0 \
-#     --binauthz-evaluation-mode​=DISABLED \
-#     --enable-managed-prometheus \
-#     --enable-shielded-nodes \
-#     --node-locations "us-central1-c"
